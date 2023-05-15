49b6d5ac8dfbe7b569e4c6197cd74f94ea02c8a6
mhasan1224 <mhassan.merit@gmail.com>
2023-05-15 22:33:10 +0600

    add more

---
+++ main.js
@@ -0,0 +1,7 @@
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click" , function(){
+    const heroSection = document.getElementById("hero-section");
+    heroSection.style.display = "none";
+    const transectionSection = document.getElementById("transection_area")
+    transectionSection.style.display = "blok";
+})

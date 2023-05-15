08b23edc7985f5b0a3f96013a544725514b22c71
mhasan1224 <mhassan.merit@gmail.com>
2023-05-16 02:34:02 +0600

    deposit done

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+//login area event handler.
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click" , function(){
     const heroSection = document.getElementById("hero-section");
     heroSection.style.display = "none";
-    const transectionSection = document.getElementById("transection_area")
-    transectionSection.style.display = "blok";
+    const transectionSection = document.getElementById("transection-section");
+    transectionSection.style.display = "block";
+})
+//transection area event handler.
+const depositBtn = document.getElementById("addAmount");
+depositBtn.addEventListener("click" , function(){
+    //console.log("drp");
+    const depositAmount = document.getElementById("amountDeposit").value;
+    const depositNumber = parseFloat(amountDeposit);
 })
